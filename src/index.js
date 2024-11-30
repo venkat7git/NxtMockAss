bb67531ab4a5c8626bef1c870b2e4e03118bedfe
venkat <venkat@gmail.com>
2024-11-30 11:09:29 +0000

    updated nxtmock

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-import {GoogleOAuthProvider} from '@react-oauth/google'
 import App from './App'
 
 ReactDOM.render(
